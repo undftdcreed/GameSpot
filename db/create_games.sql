1b95daf2c89ae8e3abab8c0cc33c36546c5b8c72
Brandonbay <undftdcreed@gmail.com>
2023-07-01 15:15:27 -0700

    second commit

---
+++ db/create_games.sql
@@ -0,0 +1,3 @@
+INSERT INTO main_app_game(name, img, bio, create_at)
+    VALUES
+        ()
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    bio = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
